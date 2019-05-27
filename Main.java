eac94720ffa0cf717174c75066e698003a6932f1
kravaukr <51087795+kravaukr@users.noreply.github.com>
2019-05-27 17:53:45 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class Main {
+
+    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException, NoSuchFieldException {
+	// write your code here
+
+        Class<?> cls = Container.class;
+        Container obj = new Container();
+        String path = cls.getAnnotation(SavaTo.class).path();
+        //String textContainer = obj.text;
+        Method[] methods = cls.getDeclaredMethods();
+        for (Method method: methods) {
+            if (method.isAnnotationPresent(Saver.class)){
+                System.out.println("Text saved!");
+                method.invoke(obj,path);
+                }
+        }
+    }
+}
