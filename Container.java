523323734c7c17ea4e3d81fdc1b6d9ab983392da
kravaukr <51087795+kravaukr@users.noreply.github.com>
2019-05-27 17:54:13 +0300

    Create Container.java

---
+++ Container.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+@SavaTo(path="D:\\1.txt")
+public class Container {
+    String text="...";
+    @Saver
+    public void save(String path) throws FileNotFoundException {
+        try (PrintWriter out = new PrintWriter(path)) {
+            out.println(text);
+        }
+    }
+}
